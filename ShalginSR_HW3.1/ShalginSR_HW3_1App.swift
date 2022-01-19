e3984f345f6667237011238a865acb6bd931e8bc
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-19 21:51:55 +0300

    Initial Commit

---
+++ ShalginSR_HW3.1/ShalginSR_HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ShalginSR_HW3_1App.swift
+//  ShalginSR_HW3.1
+//
+//  Created by Станислав on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShalginSR_HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
